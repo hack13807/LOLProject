9b02348b84a2f3886cd9c07e42c3fd78fafda0ed
wells <490864627@qq.com>
2019-09-23 11:04:50 +0800

    提交5

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -24,7 +24,7 @@ public class MybatisTest {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             session = factory.openSession();
         } catch (IOException e) {
-            //²â899
+            //wells
             e.printStackTrace();
         }
     }

b49f03a034dc123991d9041e605989e62c80aa59
wells <490864627@qq.com>
2019-09-23 10:57:21 +0800

    98

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -24,7 +24,7 @@ public class MybatisTest {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             session = factory.openSession();
         } catch (IOException e) {
-            //≤‚ ‘aabcdeasda98989
+            //≤‚8
             e.printStackTrace();
         }
     }

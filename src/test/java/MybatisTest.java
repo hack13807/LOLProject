68213322438050c12e3c7e7bfe1a3e661e1743f1
hack13807 <490864627@qq.com>
2019-09-23 10:50:58 +0800

    555
    Signed-off-by: hack13807 <490864627@qq.com>

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -24,7 +24,7 @@ public class MybatisTest {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             session = factory.openSession();
         } catch (IOException e) {
-            //≤‚ ‘aabcde
+            //≤‚ ‘aabcdeasda
             e.printStackTrace();
         }
     }

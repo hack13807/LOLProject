6cfb30b455e32304aacfa41286a7b2145b0d6445
hack13807 <490864627@qq.com>
2019-09-23 10:43:46 +0800

    提交4

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -24,7 +24,7 @@ public class MybatisTest {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             session = factory.openSession();
         } catch (IOException e) {
-            //≤‚ ‘aabc
+            //≤‚ ‘aabcde
             e.printStackTrace();
         }
     }
